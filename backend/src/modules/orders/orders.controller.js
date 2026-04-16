d353e8cadce187cd3d5a6241d7edc0174be34134
Lê Quốc Trung <142233178+Trungdzvcl012@users.noreply.github.com>
2026-04-16 15:25:53 +0700

    Update orders.controller.js

--- backend/src/modules/orders/orders.controller.js
+++ backend/src/modules/orders/orders.controller.js
@@ -4,15 +4,30 @@ const { success, failure } = require("../../shared/response");
 class OrdersController {
   async createOrder(req, res) {
     const result = await ordersService.createOrder(req.user.id, req.body);
+
     if (result.error) {
       return failure(res, result.error.message, result.error.statusCode);
     }
-    return success(res, result.data, result.message, result.statusCode || 201);
+
+    return success(res, result.data, result.message, result.statusCode);
   }
 
   async getMyOrders(req, res) {
     const result = await ordersService.getMyOrders(req.user.id);
     return success(res, result.data);
+  }
+
+  // 🔥 ADMIN
+  async getAllOrders(req, res) {
+    const result = await ordersService.getAllOrders();
+    return success(res, result.data);
+  }
+
+  async confirmOrder(req, res) {
+    const { order_number } = req.body;
+
+    const result = await ordersService.confirmOrder(order_number);
+    return success(res, "Đã xác nhận đơn");
   }
 }
 
